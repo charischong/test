80b0e27eb26cf8941e7772f68b0897c50efafe03
Charis Chong <chong.charis@gmail.com>
2021-05-30 15:03:38 +0800

    add dict

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 14:50:12 2021
+
+@author: AU000MZG
+"""
+
+class Freshie:
+    
+    freshies = { "Name" : ['Freshie1' , 'Freshie2', 'Freshie3'],
+                "Age" : [21,19,20],
+                "University" : ['SUSS', 'SUTD', 'SIT']
+        }
+    
+    def __init__(self, name):
+        self.name = name
+        
